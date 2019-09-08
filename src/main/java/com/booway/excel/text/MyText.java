487c9388b17b799f4d914ac495e764be2e2c1e43
AAAJMLAAA <48378262+AAAJMLAAA@users.noreply.github.com>
2019-09-08 11:07:09 +0800

    Update MyText.java

--- src/main/java/com/booway/excel/text/MyText.java
+++ src/main/java/com/booway/excel/text/MyText.java
@@ -5,5 +5,7 @@ public class MyText
 	public static void main(String[] args) 
 	{
 		System.out.println("OK");
+		System.out.println("OK");
+		System.out.println("OK");System.out.println("OK");
 	}
 }
